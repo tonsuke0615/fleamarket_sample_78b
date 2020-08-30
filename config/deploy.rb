df269bffa8f8e265a25d5dc0f745fc79221f0897
tonsuke0615 <daisuke.saisu@gmail.com>
2020-08-30 14:42:58 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "fleamarket_sample_78b"
-set :repo_url, "git@github.com:tonsuke0615/fleamarket_sample_78b.git”
+set :repo_url, "git@github.com:tonsuke0615/fleamarket_sample_78b.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
