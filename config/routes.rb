897fa593e11f5253c5e298b39519987293417e4b
kaji-yuki <k.yukiko.a@gmail.com>
2020-09-25 19:17:45 +0900

    change image post field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  root 'items#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
