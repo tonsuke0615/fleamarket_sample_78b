a25e672e7013d9d773dfcb7de88fe1735cbec72d
kaji-yuki <k.yukiko.a@gmail.com>
2020-09-30 12:06:22 +0900

    implemented itemTable and categoryTable

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_30_022851) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
