96946038379988cf8774f9eb1ea9408d94b0983d
tonsuke0615 <daisuke.saisu@gmail.com>
2020-09-30 21:16:08 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,18 @@ ActiveRecord::Schema.define(version: 2020_09_30_031220) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "mypages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_mypages_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_mypages_on_reset_password_token", unique: true
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
