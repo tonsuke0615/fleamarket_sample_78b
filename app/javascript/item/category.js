1b6dd56372fca58c03ecddbf9aab653422e7b222
kaji-yuki <k.yukiko.a@gmail.com>
2020-10-01 22:27:49 +0900

    implemented children_form

--- app/javascript/item/category.js
+++ app/javascript/item/category.js
@@ -2,9 +2,9 @@ window.addEventListener('load', function () {
 // 子レベルのselectタグを追加
 function add_childSelect_tag() {
   let child_select_form = `
-            <select name="item[category_id]" class="child_category_id">
-              <option value="">---</option>
-            </select>
+              <select name="item[category_id]" class="child_category_id">
+              <option value="">次のカテゴリを選択</option>
+              </select>
             `
   return child_select_form;
 }
@@ -20,18 +20,17 @@ console.log("test1")
 
 // 親カテゴリを選択したあとのイベント
 $("#category_form").on("change", function() {
-  console.log("test2")
   let parentValue = $("#category_form").val();
   if (parentValue.length != 0) {
     $.ajax({
-      url: '/items/search',
+      url: '/items/search.json',
       type: 'GET',
       data: { parent_id: parentValue},
       dataType: 'json'
     })
     .done(function (data){
       let child_select_form = add_childSelect_tag
-      $(".categoryField").append(child_select_form);
+      $(".ItemInfo__category--form").append(child_select_form);
       data.forEach(function(d){
         let option_html = add_Option(d)
         $(".child_category_id").append(option_html);
