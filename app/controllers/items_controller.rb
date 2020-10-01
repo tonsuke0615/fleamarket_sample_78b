a7bb55ae75abfeb52dda0a836f082268c26569e7
kaji-yuki <k.yukiko.a@gmail.com>
2020-10-01 12:48:08 +0900

    add javascript directory

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,10 @@ class ItemsController < ApplicationController
   end
 
   def new 
-    @parents = Category.all.order("id ASC").limit(13)
+    @item = Item.new
     # @item.item_images.new
-
+    @parents_category = Category.where(ancestry: nil) 
+    # @children = Category.find(params[:parent_id]).children
   end
   
 end
