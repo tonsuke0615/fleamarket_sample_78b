c46a79cb0cc887898cec94e99c2ec1adc0e60b28
kaji-yuki <k.yukiko.a@gmail.com>
2020-10-01 20:11:42 +0900

    test change_event of category.js

--- app/javascript/item/category.js
+++ app/javascript/item/category.js
@@ -1,4 +1,25 @@
-$("#category_form").change(function (){
+// 子レベルのselectタグを追加
+function add_childSelect_tag() {
+  let child_select_form = `
+            <select name="item[category_id]" class="child_category_id">
+              <option value="">---</option>
+            </select>
+            `
+  return child_select_form;
+}
+
+// selectタグにoptionタグを追加
+function add_Option(children) {
+  let option_html = `
+                    <option value=${children.id}>${children.name}</option>
+                    `
+  return option_html;
+}
+console.log("test1")
+
+// 親カテゴリを選択したあとのイベント
+$("#category_form").on("change", function() {
+  console.log("test2")
   let parentValue = $("#category_form").val();
   if (parentValue.length != 0) {
     $.ajax({
@@ -8,8 +29,16 @@ $("#category_form").change(function (){
       dataType: 'json'
     })
     .done(function (data){
+      let child_select_form = add_childSelect_tag
+      $(".categoryField").append(child_select_form);
+      data.forEach(function(d){
+        let option_html = add_Option(d)
+        $(".child_category_id").append(option_html);
+      })
     })
     .fail(function (){
+      alert("エラーです")
     });
   }
 });
+console.log("test3")
---
+++ app/views/items/search.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @childrens do |children|
+  json.id children.id
+  json.name children.name
+end
