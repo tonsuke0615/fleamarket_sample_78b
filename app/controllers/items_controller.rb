a31b5c559ceda8b09156ca89548fd5424ecbdee9
Soleiu <himamegu847@gmail.com>
2020-10-14 12:38:19 +0900

    yaen upgrade

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, locals: {item: new}
+    end
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def search
