fdddf941f59c16407094d702c138661ce3e632a3
kaji-yuki <k.yukiko.a@gmail.com>
2020-10-18 21:51:29 +0900

    Revert "Revert "Revert "Merge pull request #40 from tonsuke0615/出品機能の実装_テストコードとエラーメッセージ"""
    This reverts commit 4944428c30d86d781ea867c693cafd923a66fc30.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module FleamarketSample78b
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = :ja
     
     config.generators do |g|
       g.stylesheets false
