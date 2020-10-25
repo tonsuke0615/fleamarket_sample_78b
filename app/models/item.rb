65567a65a72d80d26079e6b34ad036aecbe46d36
kaji-yuki <k.yukiko.a@gmail.com>
2020-10-25 12:14:04 +0900

    remove commentout in item-table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
-  
-  # ユーザー登録が未実装のためコメントアウト
-  # belongs_to :user
+
+  belongs_to :user
   belongs_to :category
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
