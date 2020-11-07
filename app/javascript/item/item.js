9a8d8f6936ff9c1ccc7635a0b1c8520cc1444087
kaji-yuki <k.yukiko.a@gmail.com>
2020-11-07 13:20:26 +0900

    TBD_edit_function before trying other code

--- app/javascript/item/item.js
+++ app/javascript/item/item.js
@@ -3,16 +3,14 @@ window.addEventListener('load', function () {
   const buildFileField = (index)=> {
     const html = `<div class="Image-box__form" data-index="${index}">
                   <input class="Image-box__uploader" type="file" name="item[item_images_attributes][${index}][src]" id="item_item_images_attributes_${index}_src">
-                  <div class="remove">削除</div></div>`;
-    
-                  // ラベルとアイコン付のときに使っていたコード
-                  // <div class="Image-box"><div class="Image-box__top">
-                  // <label for="item_item_images_attributes_${index}_src">
-                  // <div class="Image-box__form" data-index="${index}"></div>
-                  // <input class="Image-box__uploader" type="file" name="item[item_images_attributes][${index}][src]" id="item_item_images_attributes_${index}_src">
-                  // </label></div><div class="Image-box__bottom"><span class="js-remove">削除</span></div></div>`;
-    return html;
-  }
+                  <div class="remove">削除</div>
+                  </div>`;
+
+                  // <input name="item[item_images_attributes][${index}][_destroy]" type="hidden" >
+                  // <input data-index="${index}" class="hidden" type="checkbox" value="1" name="item[item_images_attributes][${index}][_destroy]" id="item_item_images_attributes_${index}__destroy">
+
+                  return html;
+  };
 
   let fileIndex = [1,2,3];
   // // この下の２行書き足したので要検討
@@ -27,9 +25,9 @@ window.addEventListener('load', function () {
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1 )
   });
 
-  $(".hidden").hide();
-  $(document).on('click', '.remove', function() {
-    const targetIndex = $(this).parent().data('index');
+  // $(".hidden").hide();
+  $(document).on('click'  , '.remove', function() {
+    const targetIndex = $(this).parent(".Image-box__form").data('index');
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden`);
     if (hiddenCheck) hiddenCheck.prop('checked', true);
       $(this).parent().remove();
