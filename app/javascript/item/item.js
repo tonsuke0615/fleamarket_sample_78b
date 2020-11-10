76d6702b8776585668822aa70c023dbd181060ee
kaji-yuki <k.yukiko.a@gmail.com>
2020-11-11 01:20:48 +0900

    delete commentout

--- app/javascript/item/item.js
+++ app/javascript/item/item.js
@@ -14,14 +14,14 @@ window.addEventListener('load', function () {
   lastIndex = $('.Image-box__form:last').data('index');
   fileIndex.splice(0, lastIndex);
 
-  // プレビューHTML
+  // プレビュー用のHTML
   const buildImg = (index, url)=> {
     const html = `
-    <img data-index="${index}" src="${url}" width="120px" height="120px">`;
+    <img data-index="${index}" src="${url}" width="140px" height="140px">`;
     return html;
   }
 
-  // 追加した画像のプレビュー
+  // 画像のプレビューが追加される
   $(".ImageField").on('change', ".Image-box__uploader", function(e){
     const targetIndex = $(this).parent().data('index');
     console.log(targetIndex)
@@ -31,7 +31,6 @@ window.addEventListener('load', function () {
       img.setAttribute('src',blobUrl);
     } else {
       $('.Image-box__top').append(buildImg(targetIndex, blobUrl));
-
       if ($(".Image-box__uploader").length < 4  ) {
         $('.ImageField').append(buildFileField(fileIndex[0]));
         fileIndex.shift();
@@ -58,7 +57,8 @@ window.addEventListener('load', function () {
     // プレビューを消す記述
     $(`img[data-index="${targetIndex}"]`).remove();
     $(this).parent(".Image-box__form").remove();
-
+    
+    // inputタグが０にならない
     if ($(".Image-box__uploader").length == 0) $('.ImageField').append(buildFileField(fileIndex[0]));
   });
 });
