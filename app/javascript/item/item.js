41ee734c52d393ccbfc1fb0b694b4d21dff6b9c3
kaji-yuki <k.yukiko.a@gmail.com>
2020-11-11 00:51:05 +0900

    delete commentout

--- app/javascript/item/item.js
+++ app/javascript/item/item.js
@@ -1,8 +1,7 @@
 const { htmlPrefilter } = require("jquery");
 
 window.addEventListener('load', function () {
-// $(document).on('turbolinks:load', ()=> {
-  // 元々の記述
+
   const buildFileField = (index)=> {
     const html = `<div class="Image-box__form" data-index="${index}">
                   <input class="Image-box__uploader" type="file" name="item[item_images_attributes][${index}][src]" id="item_item_images_attributes_${index}_src">
@@ -11,23 +10,11 @@ window.addEventListener('load', function () {
                   return html;
   };
 
-
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // // この下の２行書き足したので要検討
   lastIndex = $('.Image-box__form:last').data('index');
   fileIndex.splice(0, lastIndex);
 
-  // // inputタグを変更すると４つまで新しいinputタグが生成される
-  // $(document).on("change", ".Image-box__uploader", function(e) {
-  //   if ($(".Image-box__uploader").length >= 4  )
-  //     return false
-  //   $(".ImageField").append(buildFileField(fileIndex[0]));
-  //   fileIndex.shift();
-  //   fileIndex.push(fileIndex[fileIndex.length - 1] + 1 )
-  // });
-
-
-  // もともと記述ここからプレビュー関連
+  // プレビューHTML
   const buildImg = (index, url)=> {
     const html = `
     <img data-index="${index}" src="${url}" width="120px" height="120px">`;
@@ -50,11 +37,8 @@ window.addEventListener('load', function () {
         fileIndex.shift();
         fileIndex.push(fileIndex[fileIndex.length - 1] + 1 )
       }
-
     };
   });
-
-// 　いま起きている現象=> JSによって呼び出されるファイルが違う場所にある
 
   $(".ImageField").on('click',".remove", function(){
     // indexを取得する
@@ -73,10 +57,8 @@ window.addEventListener('load', function () {
 
     // プレビューを消す記述
     $(`img[data-index="${targetIndex}"]`).remove();
-
     $(this).parent(".Image-box__form").remove();
 
     if ($(".Image-box__uploader").length == 0) $('.ImageField').append(buildFileField(fileIndex[0]));
   });
-
 });
