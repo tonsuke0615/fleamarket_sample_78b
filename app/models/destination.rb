960c739a7d587ddf37bc4f56730221f28c454769
Sakunori <hamu.714@gmail.com>
2020-11-12 23:04:23 +0900

    add_validates

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,8 @@
 class Destination < ApplicationRecord
   belongs_to :user, optional: true
   validates :destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefecture, :city, :address, presence: true
+  validates :destination_family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}, presence: true
+  validates :destination_first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}, presence: true
+  validates :post_code, format: { with: /\A\d{7}\z/}, presence: true
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
   validates :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_date, presence: true
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}, presence: true
 end
